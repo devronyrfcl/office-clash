39a0202f38f058134d05adb1c90823700206d6e7
Ashraful Shuvon <ashraful.shuvon@gmail.com>
2026-02-19 03:14:57 +0600

    File

--- Assets/Scripts/Network/NetworkPlayer.cs
+++ Assets/Scripts/Network/NetworkPlayer.cs
@@ -2,26 +2,33 @@ using UnityEngine;
 
 public class NetworkPlayer : MonoBehaviour
 {
-    
+
     [SerializeField] Rigidbody rigidbody3D;
 
     [SerializeField] ConfigurableJoint mainJoint;
 
     Vector2 moveInputVector = Vector2.zero;
 
-    float maxSpeed = 3f;
+    public float maxSpeed = 2f;
 
     bool isGrounded = false;
     bool isJumpButtonPressed = false;
 
     RaycastHit[] raycastHits = new RaycastHit[10];
-    
-    
-    
+
+    [SerializeField] private SyncPhysicsObject[] syncPhysicsObjects;
+
+    [SerializeField] private Animator animator;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        
+        //seach for SyncPhysicsObjects in children and add them to the list
+        if(syncPhysicsObjects.Length == 0)
+            syncPhysicsObjects = GetComponentsInChildren<SyncPhysicsObject>();
+
+
+
     }
 
     // Update is called once per frame
@@ -57,30 +64,36 @@ public class NetworkPlayer : MonoBehaviour
 
         float inputMagnitude = moveInputVector.magnitude;
 
-        if(inputMagnitude != 0f)
+        Vector3 localVelocityForward = transform.forward * Vector3.Dot(transform.forward, rigidbody3D.linearVelocity);
+
+        float localForwardVelocity = localVelocityForward.magnitude;
+
+        if (inputMagnitude != 0f)
         {
             Quaternion desireDirection = Quaternion.LookRotation(new Vector3(moveInputVector.x, 0f, moveInputVector.y * -1f), transform.up);
             mainJoint.targetRotation = Quaternion.RotateTowards(mainJoint.targetRotation, desireDirection, 300f * Time.fixedDeltaTime);
 
-            Vector3 localVelocityForward = transform.forward * Vector3.Dot(transform.forward, rigidbody3D.linearVelocity);
+            
 
-            float localForwardVelocity = localVelocityForward.magnitude;
-
-            if(localForwardVelocity < maxSpeed)
+            if (localForwardVelocity < maxSpeed)
             {
                 rigidbody3D.AddForce(transform.forward * inputMagnitude * 30f);
             }
 
-            if(isJumpButtonPressed && isGrounded)
+            if (isJumpButtonPressed && isGrounded)
             {
                 rigidbody3D.AddForce(transform.up * 20f, ForceMode.Impulse);
                 isJumpButtonPressed = false;
             }
 
-            
+            animator.SetFloat("movementSpeed", localForwardVelocity * 0.9f);
 
-
+            //update the joint rotation based on the animation
+            for (int i = 0; i < syncPhysicsObjects.Length; i++)
+            {
+                syncPhysicsObjects[i].UpdateJointAnimation();
+                Debug.Log("Updating joint animation for " + syncPhysicsObjects[i].gameObject.name);
+            }
         }
-
     }
 }
